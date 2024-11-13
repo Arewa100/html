a8fc950fd7a981c4efa295269ba857d92b09b5d4
Arewa100 <olasorexway@gmail.com>
2024-11-13 03:24:26 +0100

    update

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,27 +1,214 @@
-const  {buttonOne, buttonTwo, buttonThree, 
-buttonFour, buttonFive,buttonSix,
-buttonSeven, buttonEight, buttonNine} = require('./buttons');
+class Calculator {
+    constructor() {
+
+    }
+
+    add(firstNumber, secondNumber) {
+        return (firstNumber + secondNumber);
+    }
+
+    subtract(firstNumber, secondNumber) {
+        return (firstNumber - secondNumber);
+    }
+
+    divide(firstNumber, secondNumber){
+        return (firstNumber/secondNumber);
+    }
+
+    modulo(firstNumber, secondNumber){
+        return (firstNumber % secondNumber);
+    }
+
+    squareRoot(number) {
+        return Math.sqrt(number);
+    }
+
+}
+
+
+let calculator = new Calculator;
 
 const calculatorScreen = document.querySelector('input');
 
 console.log(calculatorScreen);
 
 
+let firstContainer = '';
+let secondContainer = '';
+let operator = null;
 
-buttonSeven.addEventListener('click', (event)=> {
+const buttonOne = document.querySelector('.one')
+const buttonTwo = document.querySelector('.two')
+const buttonThree = document.querySelector('.three')
+const buttonFour = document.querySelector('.four')
+const buttonFive = document.querySelector('.five')
+const buttonSix = document.querySelector('.six')
+const buttonSeven = document.querySelector('.seven')
+const buttonEight = document.querySelector('.eight')
+const buttonNine = document.querySelector('.nine')
+const buttonZero = document.querySelector('.zero')
+
+
+buttonOne.addEventListener('click', (event)=> {
     const result = event.target.value;
-        calculatorScreen.value += result
-    console.log('seven is working');
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+
+    console.log('button one is working')
 })
 
 
 
-// buttonOne.addEventListener('click', (event)=> {
-//     const result = event.target.value;
-//     calculatorScreen.value += result;
-//     console.log('button one is working')
-// })
+buttonTwo.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('button two is working')
+})
+
+buttonThree.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('button three is working')
+})
+
+buttonFour.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('button four is working')
+})
+
+buttonFive.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('button five is working')
+})
+
+buttonSix.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('button five is working')
+})
+
+buttonSeven.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('seven is working');
+})
+
+
+buttonEight.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('eight is working');
+})
+
+buttonNine.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('nine is working');
+})
+
+
+buttonZero.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    if(operator == null) {
+        firstContainer = firstContainer +  result;    
+    }
+    if(operator != null) {
+        secondContainer = secondContainer + result;
+        console.log(secondContainer);
+    }
+    console.log('zero is working');
+})
 
 
 
+//operators
+const plusbutton = document.querySelector('.plus')
 
+plusbutton.addEventListener('click', (event)=> {
+    const result = event.target.value;
+    calculatorScreen.value += result;
+    operator = result;
+    console.log('plus operator is working');
+})
+
+
+
+//equality operator
+const equalTo = document.querySelector('.equal')
+
+equalTo.addEventListener('click', (event)=> {
+    // const result = event.target.value;
+    let valueOne = (firstContainer);
+    let valueTwo = (secondContainer);
+    let theresult = eval(valueOne + operator + valueTwo);
+    console.log('equal operator is working');
+    calculatorScreen.value = theresult
+    firstContainer = '';
+    secondContainer = '';
+    operator = null;
+})
--- calculator/calculatorFunctions.js
+++ calculator/calculatorFunctions.js
@@ -1,17 +1,18 @@
-class Calculator {
-
-    constructor() {
-
-    }
-    
-    add(firstNumber, secondNumber) {
-            return firstNumber + secondNumber;
-    }
-
-    subtract() {}
-    multiply() {}
-    divide() {}
-    sqrt() {}
-}
-
-module.exports = Calculator;
+function add(a, b) {
+    return a + b; 
+  }
+  
+  function subtract(numberOne, numberTwo) {
+      return numberOne - numberTwo;
+  }
+  
+  function evenNumber(arr) {
+      let newArray = [];
+      for(let number of arr) {
+      if (number % 2 === 0) {
+       newArray.push(number)
+      }
+      }
+      return newArray
+  }
+ export {add, subtract, evenNumber};  
